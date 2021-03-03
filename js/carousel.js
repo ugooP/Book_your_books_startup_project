060bfde6bcd870fc57dac721b992eca94091e884
Ugo <ugo.prenat@gmail.com>
2021-03-03 14:36:19 +0100

    Carrousel fini

--- js/carousel.js
+++ js/carousel.js
@@ -1,1 +1,68 @@
-console.log('yo');
+const carousel = document.querySelector('.carousel')
+const slides = document.querySelectorAll('.carousel-item')
+const dots = document.querySelectorAll('.dots-container a')
+const nextSlideBtn = document.querySelector('.next-slide')
+const prevSlideBtn = document.querySelector('.prev-slide')
+
+nextSlideBtn.addEventListener('click', () => {
+    const slideWidth = carousel.getBoundingClientRect().width
+    const index = getActiveSlide()
+    const toScroll = index == 0 ? slideWidth : (index + 1) * slideWidth
+
+    if (index < slides.length - 1) {
+        carousel.scroll({
+            top: 0,
+            left: toScroll,
+            behavior: 'smooth'
+        })
+    } else {
+        carousel.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
+
+})
+
+prevSlideBtn.addEventListener('click', () => {
+    const slideWidth = carousel.getBoundingClientRect().width
+    const index = getActiveSlide()
+    const toScroll = (index - 1) * slideWidth
+
+    if (index > 0) {
+        carousel.scroll({
+            top: 0,
+            left: toScroll,
+            behavior: 'smooth'
+        })
+    } else {
+        carousel.scroll({
+            top: 0,
+            left: slides.length * slideWidth,
+            behavior: 'smooth'
+        })
+    }
+})
+
+
+function getActiveSlide() {
+    for (let i = 0; i < slides.length; i++) {
+        const bounds = slides[i].getBoundingClientRect();
+        if (bounds.x < 1 && bounds.x > -1) {
+            return i
+        }
+    }
+}
+
+function goToSlide(n) {
+    // Remove the previous active dot
+    for (let i = 0; i < dots.length; i++) {
+        if (dots[i].classList.contains('active')) {
+            dots[i].classList.remove('active')
+            break
+        }
+    }
+
+    dots[n-1].classList.add('active')
+}
