c02430f539372fd8f609271f25091c8bb94653f0
Ugo <ugo.prenat@gmail.com>
2021-03-03 18:54:17 +0100

    Avec jQuery c'est la belle vie -> section "nos prestations" finie

--- js/prestations.js
+++ js/prestations.js
@@ -1,9 +1,11 @@
-const servicesCards = document.querySelectorAll('.service')
-const servicesDescriptions = document.querySelectorAll('.service .service-description')
+const descriptions = $('.service-description')
 
-for (let i = 0; i < servicesCards.length; i++) {
-    const service = servicesCards[i];
-    service.addEventListener('click', () => {
-        service.querySelector('.service-description').classList.toggle('show-service-description')
+/* for (let i = 0; i < $('.service').length; i++) {
+    $('.service')[i].addEventListener('click', () => {
+        $('.service-description')[i].slideToggle()
     })
-}
+} */
+
+$('.service1').click(() => $('.description1').slideToggle())
+$('.service2').click(() => $('.description2').slideToggle())
+$('.service3').click(() => $('.description3').slideToggle())
